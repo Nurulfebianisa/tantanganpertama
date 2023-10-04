9bcc35729123b12e75dcea94e86f704911d21bb3
Nurul Febi Anisa <92684819+Nurulfebianisa@users.noreply.github.com>
2023-10-04 14:19:29 +0700

    Add files via upload

---
+++ 2.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<int> datalistku = [4, 7, 8, 2, 6, 3, 6, 78, 109];
+
+  // Mengurutkan list secara descending (dari yang terbesar ke yang terkecil)
+  datalistku.sort((a, b) => a.compareTo(b));
+
+  print('$datalistku');
+}
